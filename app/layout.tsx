36318eb96611ac250b9e6a3ee3496b277e0571ab
afankulaglic <afankulaglic@outlook.com>
2025-11-07 18:23:48 +0100

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,11 @@
 import "./globals.css";
 import Link from "next/link";
 
+export const metadata = {
+  title: "Pameto odabrano",
+  description: "Next.js app sa Navbarom i Footerom",
+};
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
